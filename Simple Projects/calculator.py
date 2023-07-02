aa1749f9a1d17bb9ecd433d3b5b2eade85016191
U.Senthil Kumar <U.senthilkumar1@outlook.com>
2023-07-02 21:44:57 +0530

    simple calculator
    A small Calculator code while learning basics of python

---
+++ Simple Projects/calculator.py
@@ -0,0 +1,17 @@
+first = input("first number: ")
+operator = input("enter operator: ")
+second = input("second number: ")
+
+first = int(first)
+second = int(second)
+
+if operator == "+":
+    print(first + second)
+elif operator == "-":
+    print(first - second)
+elif operator == "*":
+    print(first * second)
+elif operator == "%":
+    print(first % second)
+else:
+    print("invalid")
